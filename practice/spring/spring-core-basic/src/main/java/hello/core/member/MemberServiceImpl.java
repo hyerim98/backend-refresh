cbd435e95a7c332ca32a6971bcb4b2b1c493154e
hyerim98 <choihyerim252@gmail.com>
2026-01-30 20:16:56 +0900

    feat: 스프링 핵심 원리 섹션3 예제 구현
    - 회원, 주문 도메인 예제 추가
    - 설계 의도와 한계점 README에 정리

---
+++ practice/spring/spring-core-basic/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    // 구현체 의존 DIP 위반
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long id) {
+        return memberRepository.findById(id);
+    }
+}
