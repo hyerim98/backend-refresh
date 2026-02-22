570c14ae3f296c0dd26e7ef157429db128909e94
hyerim98 <choihyerim252@gmail.com>
2026-02-22 14:09:03 +0900

    feat: 섹션2 타임리프 기본 기능 및 렌더링 구조 정리

---
+++ practice/spring/spring-mvc2-thymeleaf/src/test/java/hello/thymeleaf/SpringMvc2ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
